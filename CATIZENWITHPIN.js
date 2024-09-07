dd01ae8494d63540cdb7412b658afe4f8ec98e09
bartanaw <137281633+bartanaw@users.noreply.github.com>
2024-09-07 10:10:49 +0600

    Update CATIZENWITHPIN.js

--- CATIZENWITHPIN.js
+++ CATIZENWITHPIN.js
@@ -1,68 +1,68 @@
-const correctPasswordEncoded = "QEFzSGlrNzA1"; 
-const userPassword = prompt("Enter Your Passsword, admin: https://www.facebook.com/AsHik705 :");
-const userPasswordEncoded = btoa(userPassword);
-if (userPasswordEncoded === correctPasswordEncoded) {
-  const consoleRed = "font-weight: bold; color: red;";
-  const consoleGreen = "font-weight: bold; color: green;";
-  const consolePrefix = "%c [AutoBot] ";
-  console.clear();
-  console.log(`${consolePrefix} inserting... `, consoleGreen);
-  try {
-    function onClickAuto() {
-      P.cat.isAuto = !P.cat.isAuto;
-      if (P.cat.isAuto) {
-        this.ani8.play(0, true);
-        Laya.timer.loop(500, this, this.checkAuto);
-        this.checkFreeCat();
-        this.m_btn_AutoSetting.visible = true;
-      } else {
-        Laya.timer.clearAll(this.checkAuto);
-        this.ani8.stop();
-        Laya.timer.loop(5000, this, this.checkSum);
-        this.m_btn_AutoSetting.visible = false;
-      }
-      this.m_img_StopAuto.visible = !P.cat.isAuto;
-      u(
-        `AutoBot ${
-          P.cat.isAuto ? " activated " : " turned off"
-        }!\n\n Cracked by @BartaNawOfficial `
-      );
-    }
-    //Assign the onClickAuto function to N's prototype instead
-    N.prototype.onClickAuto = onClickAuto;
-    console.log(`${consolePrefix} Script loaded `, consoleGreen);
-    console.log(`${consolePrefix} Crack by BartaNawOfficial `, consoleGreen);
-  } catch (e) {
-    console.log(
-      `${consolePrefix}An error occurred, the breakpoint was not set correctly!`,
-      consoleRed
-    );
-    console.log(
-      `${consolePrefix}Please follow the instructions, and you will succeed :*`,
-      consoleRed
-    );
-  }
-  function decodeBase64(encodedText) {
-    const decodedBytes = atob(encodedText);
-    return decodeURIComponent(escape(decodedBytes));
-  }
-  const encodedText = "ICBBdXRob3IgQ2hhbm5lbDog aHR0cHM6Ly90Lm1lL0JhcnRh TmF3T2ZmaWNpYWwg"; 
-  const encodedtext = "IEF1dGhvciBMaW5rOiBodHRwczovL3d3dy5mYWNlYm9vay5jb20vQXNIaWs3MDU=";
+const _0x4e63d7 = "QEFzSGlrNzA1"; 
+const _0x191796 = prompt("Enter Your Passsword, admin: https://www.facebook.com/AsHik705 :");
+const _0x588343 = btoa(_0x191796);
+if (_0x588343 === _0x4e63d7) {
+  const _0x3d007d = "font-weight: bold; color: red;";
+  const _0x54054d = "font-weight: bold; color: green;";
+  const _0x4f2050 = "%c [AutoBot] ";
+  console.clear();
+  console.log(`${_0x4f2050} inserting... `, _0x54054d);
+  try {
+    function _0x36c22e() {
+      P.cat.isAuto = !P.cat.isAuto;
+      if (P.cat.isAuto) {
+        this.ani8.play(0, true);
+        Laya.timer.loop(500, this, this.checkAuto);
+        this.checkFreeCat();
+        this.m_btn_AutoSetting.visible = true;
+      } else {
+        Laya.timer.clearAll(this.checkAuto);
+        this.ani8.stop();
+        Laya.timer.loop(5000, this, this.checkSum);
+        this.m_btn_AutoSetting.visible = false;
+      }
+      this.m_img_StopAuto.visible = !P.cat.isAuto;
+      u(
+        `AutoBot ${
+          P.cat.isAuto ? " activated " : " turned off"
+        }!\n\n Cracked by @BartaNawOfficial `
+      );
+    }
+    //Assign the _0x36c22e function to N's prototype instead
+    N.prototype.onClickAuto = _0x36c22e;
+    console.log(`${_0x4f2050} Script loaded `, _0x54054d);
+    console.log(`${_0x4f2050} Crack by BartaNawOfficial `, _0x54054d);
+  } catch (e) {
+    console.log(
+      `${_0x4f2050}An error occurred, the breakpoint was not set correctly!`,
+      _0x3d007d
+    );
+    console.log(
+      `${_0x4f2050}Please follow the instructions, and you will succeed :*`,
+      _0x3d007d
+    );
+  }
+  function _0x2f46(_0x1c3d5e) {
+    const _0x139711 = atob(_0x1c3d5e);
+    return decodeURIComponent(escape(_0x139711));
+  }
+  const _0x30c40a = "ICBBdXRob3IgQ2hhbm5lbDog aHR0cHM6Ly90Lm1lL0JhcnRh TmF3T2ZmaWNpYWwg"; 
+  const _0x5a57 = "IEF1dGhvciBMaW5rOiBodHRwczovL3d3dy5mYWNlYm9vay5jb20vQXNIaWs3MDU=";
 
-  const decodedText = decodeBase64(encodedText);
-  const decodedtext = decodeBase64(encodedtext)
-  console.log(decodedText);
-  console.log(decodedtext)
+  const _0x55f3d6 = _0x2f46(_0x30c40a);
+  const _0x334ae3 = _0x2f46(_0x5a57)
+  console.log(_0x55f3d6);
+  console.log(_0x334ae3)
 } else {
-  console.log("Wrong, Try Again।");
-  function decodeBase64(encodedText) {
-    const decodedBytes = atob(encodedText);
-    return decodeURIComponent(escape(decodedBytes));
-  }
-  const encodedText = "ICBBdXRob3IgQ2hhbm5lbDog aHR0cHM6Ly90Lm1lL0JhcnRh TmF3T2ZmaWNpYWwg"; 
-  const encodedtext = "IEF1dGhvciBMaW5rOiBodHRwczovL3d3dy5mYWNlYm9vay5jb20vQXNIaWs3MDU=";
-  const decodedText = decodeBase64(encodedText);
-  const decodedtext = decodeBase64(encodedtext)
-  console.log(decodedText);
-  console.log(decodedtext)
+  console.log("Wrong, Try Again।");
+  function _0x2f46(_0x1c3d5e) {
+    const _0x139711 = atob(_0x1c3d5e);
+    return decodeURIComponent(escape(_0x139711));
+  }
+  const _0x30c40a = "ICBBdXRob3IgQ2hhbm5lbDog aHR0cHM6Ly90Lm1lL0JhcnRh TmF3T2ZmaWNpYWwg"; 
+  const _0x5a57 = "IEF1dGhvciBMaW5rOiBodHRwczovL3d3dy5mYWNlYm9vay5jb20vQXNIaWs3MDU=";
+  const _0x55f3d6 = _0x2f46(_0x30c40a);
+  const _0x334ae3 = _0x2f46(_0x5a57)
+  console.log(_0x55f3d6);
+  console.log(_0x334ae3)
 }
